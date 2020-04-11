02ae5e4ec8fe235a703b8a6adbcb67162bcd249b
George Hart <george.titta@gmail.com>
2020-04-11 09:50:26 +0200

    add description
    Cranio-Corpo-Graphy (CCG) is a medical investigation and measurement procedure developed in 1968 by German neurootologist Claus-Frenz Claussen. 
    It documents and evaluates disorders of the equilibrium function measured by investigation procedures such as the 
    Unterberger test, the LOLAVHESLIT test, the NEFERT test, the Romberg's test and the WOFEC test.

--- GH_CCG_2020.py
+++ GH_CCG_2020.py
@@ -11,6 +11,10 @@ GitHub          :
 
 Description     :
 
+Cranio-Corpo-Graphy (CCG) is a medical investigation and measurement procedure developed in 1968 by German neurootologist Claus-Frenz Claussen. 
+It documents and evaluates disorders of the equilibrium function measured by investigation procedures such as the 
+Unterberger test, the LOLAVHESLIT test, the NEFERT test, the Romberg's test and the WOFEC test.
+
 
 
 
